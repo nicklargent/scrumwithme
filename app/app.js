273d6a86dc2c0283dadd06b3777616ec9d1125e8
Matt Murphy <matt.murphy@coxautoinc.com>
2017-10-18 21:42:54 -0400

    Add fist of five

--- app/app.js
+++ app/app.js
@@ -62,7 +62,7 @@ io.sockets.on('connection', function (socket) {
                 sid: data.sid,
                 activity: new Date(),
                 users: {},
-                roomType: 'planning_poker',  // planning_poker, tshirt_sizing, relative_sizing, value_pointing, multiple_choice
+                roomType: 'planning_poker',  // planning_poker, tshirt_sizing, relative_sizing, value_pointing, multiple_choice, fist_of_five
                 hostSocket: null
             };
         }
